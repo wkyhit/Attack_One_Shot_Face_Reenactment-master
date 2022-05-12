4794f4b214aa54f8ff95771810c67657d83059cb
enity <enitykyw@outlook.com>
2022-05-13 00:10:37 +0800

    "fix iterative bug"

--- attacks.py
+++ attacks.py
@@ -33,8 +33,9 @@ class IFGSMAttack(object):
 
         if self.rand:
             X_nat['img_src'] = X_nat['img_src'].to(self.device)
-            x_tmp = X_nat['img_src']+torch.tensor(np.random.uniform(-self.epsilon, self.epsilon, X_nat['img_src'].shape).astype('float32')).to(self.device)
-            # X_nat['img_src'] += torch.tensor(np.random.uniform(-self.epsilon, self.epsilon, X_nat['img_src'].shape).astype('float32')).to(self.device)
+            random = torch.rand_like(origin_img_src).uniform_(-self.epsilon,self.epsilon).to(self.device)
+            x_tmp = X_nat['img_src'] + random
+            # x_tmp = X_nat['img_src']+torch.tensor(np.random.uniform(-self.epsilon, self.epsilon, X_nat['img_src'].shape).astype('float32')).to(self.device)
             # use the following if FGSM or I-FGSM and random seeds are fixed
             # X = X_nat.clone().detach_() + torch.tensor(np.random.uniform(-0.001, 0.001, X_nat.shape).astype('float32')).cuda()    
             X_nat['img_src'] = x_tmp.clone().detach_()
@@ -57,17 +58,26 @@ class IFGSMAttack(object):
 
             # Output attack
             # Minus in the loss means "towards" and plus means "away from"
-            loss = self.loss_fn(output, y)
+            # use mse loss
+            # loss = self.loss_fn(output, y)
+
+            #try self define loss
+            loss = ((output - y)**2).sum()
+            loss = loss.mean()
+
             loss.requires_grad_(True) #!!解决无grad bug
             loss.backward()
-            grad = self.model.img_src.grad
+            # grad = self.model.img_src.grad
+            grad = self.model.img_src.grad.data
 
             # img_src_adv = self.model.img_src + self.a * grad.sign()
             img_src_adv = self.model.img_src + self.a * torch.sign(grad)
 
-
             eta = torch.clamp(img_src_adv - origin_img_src, min=-self.epsilon, max=self.epsilon)#加入的噪声
             X = torch.clamp(origin_img_src + eta, min=-1, max=1).detach_()#攻击后的img_src结果
+
+            #重新设置攻击后的img_src给model
+            self.model.img_src = X
 
             # Debug
             # X_adv, loss, grad, output_att, output_img = None, None, None, None, None
